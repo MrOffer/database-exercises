e4773bec2d7fdf4a9a06fa5c2284a7e116a31c7b
MrOffer <thomas.e.offer@gmail.com>
2019-12-19 14:48:20 -0600

    finished everything up tp indexes

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,5 +1,4 @@
 USE codeup_test_db;
-TRUNCATE albums;
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ('Michael jackson', 'Thriller', 1982, 47.3, 'Pop'),
 ('AC/DC', 'Back in Black', 1980, 26.4, 'Hard Rock'),
