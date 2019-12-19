03d6318cbcb61f5bfe9b49ef8428ab136a3ada80
MrOffer <thomas.e.offer@gmail.com>
2019-12-19 11:40:17 -0600

    finished exercises

---
+++ functions_exercises.sql
@@ -0,0 +1,50 @@
+USE employees;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' ORDER BY emp_no;
+
+
+# Now reverse the sort order for both queries.
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' ORDER BY emp_no DESC ;
+
+# Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+
+SELECT emp_no, first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date
+    LIKE '199%'
+  AND birth_date
+    LIKE '%-12-25' ORDER BY birth_date ASC, hire_date DESC;
+
+
+# Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%e' ;
+
+# For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
+SELECT first_name, last_name, hire_date, curdate() AS 'Current Date', datediff(curdate(), hire_date) AS 'Days worked at company'
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25' ORDER BY birth_date, hire_date DESC;
--- where_exercises.sql
+++ where_exercises.sql
@@ -6,7 +6,7 @@ SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR f
 
 # Find all employees whose last name starts with 'E' — 7,330 rows.
 
-# SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name Like '%e';
+# SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name Like '%e';
 
 # Find all employees hired in the 90s — 135,214 rows.
 
@@ -14,7 +14,7 @@ SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR f
 
 # Find all employees born on Christmas — 842 rows.
 
-# SELECT * FROM employees WHERE birth_date = 12-25;
+# SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
 
 # Find all employees with a 'q' in their last name — 1,873 rows.
 
